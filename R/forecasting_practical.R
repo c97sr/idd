a8a7542e8c880c1f29a7bc08c3270cdfc46ff665
Ada Yan <a.yan@imperial.ac.uk>
2018-02-02 13:54:18 +0000

    changing plotting for linear regression

--- R/forecasting_practical.R
+++ R/forecasting_practical.R
@@ -1,3 +1,20 @@
+# Copyright Steven Riley (sr@stevenriley.net)
+#
+# This file is part of the library idd.
+#
+# idd is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This work is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this idsource.  If not, see <http://www.gnu.org/licenses/>.
+
 #' extracts the incidence data for a given country and year
 #' 
 #' \code{extract_incidence} takes the flu incidence matrix and extracts
@@ -68,16 +85,35 @@ plot_incidence <- function(incidence_data, log_scale = FALSE) {
   return(g)
 }
 
+#' subset incidence data for weeks of interest
+#' 
+#' \code{subset_incidence_data} takes the incidence data and subsets it for the weeks of interest.
+#' 
+#' @param incidence_data data frame extracted by \code{extract_incidence}
+#' @param current_week numeric vector of length 1: week number of the current week
+#' @param n_weeks_prior numeric vector of length 1: number of weeks' previous data to use
+#' @return data frame for weeks of interest
+#' @export
+#' 
+subset_incidence_data <- function(incidence_data, current_week, n_weeks_prior) {
+  week_string <- as.character(current_week)
+  if(current_week < 10) {
+    week_string <- paste0("0", week_string)
+  }
+  end_row <- grep(paste0("-", week_string), incidence_data$time_name)
+  start_row <- end_row - n_weeks_prior
+  incidence_data <- incidence_data[seq(start_row, end_row),]
+  return(incidence_data)
+}
+
 #' performs linear regression for a given country and year
 #' 
-#' \code{linear_regression} takes the flu incidence matrix and performs
-#' linear regression for a given flu season and a given country.
+#' \code{linear_regression} takes the incidence data (already subsetted for 
+#' country and year) and performs linear regression.
 #' We also specify the "current week" and how many previous weeks' worth of data
 #' to use.
 #' 
-#' @param flu_data matrix with flu incidence data
-#' @param country_code character vector of length 1: 3-letter country code
-#' @param year numeric vector of length 1: year to plot
+#' @param incidence_data data frame extracted by \code{extract_incidence}
 #' @param current_week numeric vector of length 1: week number of the current week
 #' @param n_weeks_prior numeric vector of length 1: number of weeks' previous data to use
 #' @param log_transform logical vector of length 1: if TRUE, perform linear 
@@ -86,20 +122,14 @@ plot_incidence <- function(incidence_data, log_scale = FALSE) {
 #' @return linear regression object (see documentation for \code{lm})
 #' @export
 #' 
-linear_regression <- function(flu_data,
-                              country_code = "USA",
-                              year = 2016,
+linear_regression <- function(incidence_data,
                               current_week = 47,
                               n_weeks_prior = 4,
                               log_transform = TRUE) {
-  incidence_data <- extract_incidence(flu_data, country_code, year)
-  week_string <- as.character(current_week)
-  if(current_week < 10) {
-    week_string <- paste0("0", week_string)
-  }
-  end_row <- grep(paste0("-", week_string), incidence_data$time_name)
-  start_row <- end_row - n_weeks_prior
-  incidence_data <- incidence_data[seq(start_row, end_row),]
+
+  incidence_data <- subset_incidence_data(incidence_data, current_week,
+                                          n_weeks_prior)
+  
   if(log_transform) {
     incidence_data$incidence <- log(incidence_data$incidence)
   }
@@ -116,27 +146,108 @@ linear_regression <- function(flu_data,
 #' 
 #' @param lm_output linear regression object returned by \code{linear_regression}
 #' @param incidence_data incidence data extracted by \code{extract_incidence}
+#' @param weeks_ahead numeric vector of length 1: number of weeks to forecast ahead
 #' @param log_transform logical vector of length 1: if TRUE, linear 
 #' regression was performed on log transformed data, otherwise linear regression
 #' was performed on original data
 #' @return data frame with predicted points and original data
 #' @export
 #' 
-extract_forecasted_points <- function(lm_output, incidence_data, log_transform) {
+extract_forecasted_points <- function(lm_output, incidence_data, weeks_ahead,
+                                      log_transform) {
   
   intercept <- lm_output$coefficients[["(Intercept)"]]
   gradient <- lm_output$coefficients[["t"]]
   last_timepoint_used <- lm_output$model$t[length(lm_output$model$t)]
-  times_to_forecast <- seq(last_timepoint_used + 1, nrow(incidence_data))
+  if(missing(weeks_ahead)) {
+    weeks_ahead <- nrow(incidence_data) - last_timepoint_used
+  }
+  times_to_forecast <- seq_len(weeks_ahead) + last_timepoint_used
   forecasted_points <- intercept + gradient * times_to_forecast
   if(log_transform) {
     forecasted_points <- exp(forecasted_points)
   }
-  forecasted_points <- c(rep(NA, last_timepoint_used), forecasted_points)
   
   time_used_to_forecast <- rep(FALSE, nrow(incidence_data))
   time_used_to_forecast[lm_output$model$t] <- TRUE
   incidence_data$time_used_to_forecast <- time_used_to_forecast
-  incidence_data$forecast <- forecasted_points
-  incidence_data
+  incidence_data$forecast <- rep(NA, nrow(incidence_data))
+  incidence_data$forecast[last_timepoint_used + seq_len(weeks_ahead)] <- forecasted_points
+  return(incidence_data)
 }
+
+#' calculate log likelihood of data given model prediction (assuming Poisson intensity)
+#' 
+#' \code{calc_log_likelihood} calculates the log likelihood of the incidence data 
+#' given model prediction (assuming Poisson intensity)
+#' 
+#' @param data numeric vector: incidence data
+#' @param model_prediction numeric vector of same length as \code{data}: 
+#' incidence according to model 
+#' @return log likelihood: numeric vector of length 1
+#' @export
+#' 
+calc_log_likelihood <- function(data, model_prediction) {
+  stopifnot(length(data) == length(model_prediction) &&
+              identical(data, round(data)))
+  log_likelihood <- sum(dpois(data, model_prediction, log = TRUE))
+  return(log_likelihood)
+}
+
+#' simplified version of SEIR model with default inputs
+#' 
+#' \code{simple_comp_seir} is a simplified version of \code{comp_seir}
+#' with default inputs
+#' 
+#' @param R_0 numeric vector of length 1: basic reproduction number
+#' @return A list of two elements. The first is the incidence of infection
+#' and the second is the timepoints to which the incidence refers.
+#' @export
+simple_comp_seir <- function(R_0) {
+  latent_period <- 1.6 # Cori et al. (2012) 10.1016/j.epidem.2012.06.001
+  infectious_period <- 1 # Cori et al. (2012) 10.1016/j.epidem.2012.06.001
+  pop_size <- 323.1e6 # USA population size 2016
+  model_output <- comp_seir(De=latent_period,
+            Tg=infectious_period + latent_period,
+            R0=R_0,
+            N=pop_size,
+            I0=10,
+            trickle=0,
+            trickleStart=0,
+            reprate=1,
+            dt=1,
+            R1=R_0,
+            t1=999,
+            R2=R_0,
+            t2=9999,
+            noTimeSteps=360,
+            noReals=1,
+            A=0,
+            deterministic=TRUE
+  )
+  return(model_output[c(1,2)])
+}
+
+#' #' fit the SEIR model to data
+#' #' 
+#' #' \code{fit_seir} fits an SEIR model to incidence data
+#' #' 
+#' #' @param incidence_data data frame extracted by \code{extract_incidence}
+#' #' @param current_week numeric vector of length 1: week number of the current week
+#' #' @param n_weeks_prior numeric vector of length 1: number of weeks' previous data to use
+#' #' @return A list of two elements. The first is the incidence of infection
+#' #' and the second is the timepoints to which the incidence refers.
+#' #' @export
+#' fit_seir <- function(incidence_data, current_week, n_weeks_prior) {
+#'   
+#'   evaluate_model_and_calc_log_likelihood <- function(R_0) {
+#'     model_output <- simple_comp_seir(R_0)
+#'     model_prediction <- model_output$inf_inc[seq(1, n_weeks_prior * 7 + 1, by = 7)]
+#'     log_likelihood <- calc_log_likelihood(data, model_prediction)
+#'   }
+#'   
+#'   incidence_data <- subset_incidence_data(incidence_data, current_week,
+#'                                           n_weeks_prior)
+#'   browser()
+#'   return(NULL)
+#' }
--- forecasting_run_me.R
+++ forecasting_run_me.R
@@ -12,16 +12,15 @@ data_figure <- plot_incidence(incidence_data, log_scale = FALSE)
 
 # perform linear regression starting from calendar week 47,
 # using data from 4 weeks prior
-linear_regression_output <- linear_regression(fluIliCountryData,
-                              country_code = "USA",
-                              year = 2016,
+linear_regression_output <- linear_regression(incidence_data,
                               current_week = 47,
                               n_weeks_prior = 4,
                               log_transform = FALSE)
 
-# extract forecasted points from linear regression
+# extract forecasted points from linear regression -- forecast 10 weeks ahead
 forecast_df <- extract_forecasted_points(linear_regression_output, 
                                          incidence_data,
+                                         weeks_ahead = 10,
                                          log_transform = FALSE)
 
 # plot forecast
@@ -29,16 +28,15 @@ linear_regression_4_weeks_figure <- plot_incidence(forecast_df)
 
 # perform linear regression starting from calendar week 47,
 # using data from 8 weeks prior
-linear_regression_output <- linear_regression(fluIliCountryData,
-                                              country_code = "USA",
-                                              year = 2016,
+linear_regression_output <- linear_regression(incidence_data,
                                               current_week = 47,
                                               n_weeks_prior = 8,
                                               log_transform = FALSE)
 
-# extract forecasted points from linear regression
+# extract forecasted points from linear regression, forecasting 10 weeks ahead
 forecast_df <- extract_forecasted_points(linear_regression_output, 
                                          incidence_data,
+                                         weeks_ahead = 10,
                                          log_transform = FALSE)
 
 # plot forecast
@@ -46,16 +44,15 @@ linear_regression_8_weeks_figure <- plot_incidence(forecast_df)
 
 # perform linear regression starting from calendar week 47,
 # using data from 8 weeks prior, log transforming data
-linear_regression_output <- linear_regression(fluIliCountryData,
-                                              country_code = "USA",
-                                              year = 2016,
+linear_regression_output <- linear_regression(incidence_data,
                                               current_week = 47,
                                               n_weeks_prior = 8,
                                               log_transform = TRUE)
 
-# extract forecasted points from linear regression
+# extract forecasted points from linear regression, forecasting 10 weeks ahead
 forecast_df <- extract_forecasted_points(linear_regression_output, 
                                          incidence_data,
+                                         weeks_ahead = 10,
                                          log_transform = TRUE)
 
 # plot forecast
