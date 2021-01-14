2e23815826fc8186c98e2f16a411c4da5f04e4e1
Steven <sr@stevenriley.net>
2021-01-14 21:05:13 +0000

    getting ready to restart

---
+++ R/cov_react_funcs.r
@@ -0,0 +1,27 @@
+#' Define a function to take a table and add in binomial estimates and
+#' CIs
+add.CIs <- function(tab,method="wilson") {
+    rtn <- tab
+    tmp <- dim(tab)
+    nrows <- tmp[1]
+    ncols <- tmp[2]
+    rowP <- vector(mode="numeric", length=ncols)
+    rowUB <- vector(mode="numeric", length=ncols)
+    rowLB <- vector(mode="numeric", length=ncols)
+    for (i in 1:ncols) {
+        tmpbin <- propCI(x = tab["Detected",i],
+                         n = tab["Detected",i] + tab["Not Detected",i],
+                         method=method)
+        rowP[i] <- tmpbin$p
+        rowUB[i] <- tmpbin$upper
+        rowLB[i] <- tmpbin$lower
+    }
+    rtn <- rbind(rtn, p=rowP, ub=rowUB, lb=rowLB)
+    rtn
+}
+
+sim_las <- function(n_la=315,n_sim, max_per = 10) {
+  
+  mat_sim <- matrix()
+  
+}
